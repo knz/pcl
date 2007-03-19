42b86408fcf087f83ed52f2026eb99a842451777
Davide Libenzi <davidel@xmailserver.org>
2007-03-19 18:37:01 +0000

    HTML style
    HTML style
    
    git-archimport-id: davide@xmailserver.org--v1/libpcl--ml--0.1--patch-22

--- man/mkpages.sh
+++ man/mkpages.sh
@@ -2,5 +2,5 @@
 
 groff -t -e -mandoc -Tascii pcl.3 | col -bx > pcl.txt
 groff -t -e -mandoc -Tps pcl.3 > pcl.ps
-man2html < pcl.3 | sed 's/<BODY>/<BODY text="#0000FF" bgcolor="#FFFFFF">/' > pcl.html
+man2html < pcl.3 | sed 's/<BODY>/<BODY text="#0000FF" bgcolor="#FFFFFF" style="font-family: monospace;">/' > pcl.html
 
